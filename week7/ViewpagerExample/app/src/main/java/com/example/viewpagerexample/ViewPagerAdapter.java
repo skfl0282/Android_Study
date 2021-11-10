52b434ff90a92d9f8bd9275556a724d409559fde
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2021-11-10 22:04:56 +0900

    week7

---
+++ week7/ViewpagerExample/app/src/main/java/com/example/viewpagerexample/ViewPagerAdapter.java
@@ -0,0 +1,48 @@
+package com.example.viewpagerexample;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return FragMonday.newInstance();
+            case 1:
+                return FragTuesday.newInstance();
+            case 2:
+                return FragWednesday.newInstance();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Monday";
+            case 1:
+                return "Tuesday";
+            case 2:
+                return "Wednesday";
+            default:
+                return null;
+        }
+    }
+}
